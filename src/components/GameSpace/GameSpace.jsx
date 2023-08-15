7b2b42f959ad9fb6af658b6b415fa55e1bf38495
MarcoValeri <marcovaleri@hotmail.it>
2023-08-15 16:08:09 +0100

    Add new steps

--- src/components/GameSpace/GameSpace.jsx
+++ src/components/GameSpace/GameSpace.jsx
@@ -11,6 +11,36 @@ const GameSpace = () => {
             <Panel number={1} />
             <Panel number={2} />
             <Panel number={3} />
+            <Panel number={4} />
+            <Panel number={5} />
+            <Panel number={6} />
+            <Panel number={7} />
+            <Panel number={8} />
+            <Panel number={9} />
+            <Panel number={10} />
+            <Panel number={11} />
+            <Panel number={12} />
+            <Panel number={13} />
+            <Panel number={14} />
+            <Panel number={15} />
+            <Panel number={16} />
+            <Panel number={17} />
+            <Panel number={18} />
+            <Panel number={19} />
+            <Panel number={20} />
+            <Panel number={21} />
+            <Panel number={22} />
+            <Panel number={23} />
+            <Panel number={24} />
+            <Panel number={25} />
+            <Panel number={26} />
+            <Panel number={27} />
+            <Panel number={28} />
+            <Panel number={29} />
+            <Panel number={30} />
+            <Panel number={31} />
+            <Panel number={32} />
+            <Panel number={33} />
         </div>
     )
 }
