db2dc763b9e7a48e43510701507a0e45a2034043
MarcoValeri <marcovaleri@hotmail.it>
2023-08-15 12:51:08 +0100

    Add component user

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Nav from './components/Nav/Nav'
 
 import './App.css'
 import './assets/scss/main.scss';
+import User from './components/User/User';
 
 const App = () => {
     return (
         <>
             <Nav />
+            <User name="MV" />
         </>
     )
 }
---
+++ src/components/User/User.jsx
@@ -0,0 +1,11 @@
+import './User.scss';
+
+const User = props => {
+    return (
+        <div>
+            <h2>{props.name}</h2>
+        </div>
+    )
+}
+
+export default User;
